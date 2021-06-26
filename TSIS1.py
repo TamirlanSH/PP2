eb6ff11e4142dd493218e4f9f20b1066607c9892
TamirlanSH <you@example.com>
2021-06-26 13:45:56 +0600

    TSIS1 change#1

---
+++ TSIS1.py
@@ -0,0 +1,81 @@
+#PYTHON SYNTAX
+
+#Exercise 1 (print)
+print("Hello, world!")
+#Exercise 2 (if)
+if 5 > 2:
+    print("Five is greater than two")
+
+#PYTHON COMMENTS
+
+#Exercise 1
+#This is comments
+#Exercise 2
+"""
+One
+Two
+Three
+"""
+
+#PYTHON VARIABLES
+
+#Exercise 1
+carname = "Volvo"
+#Exercise 2
+x = 50
+#Exercise 3
+x = 5
+y = 10
+print(x + y)
+#Exercise 4
+x = 5
+y = 10
+z = x + y
+print(z)
+#Exercise 5
+myfirst_name = "John"
+#Exercise 6
+x = y = z = "Orange"
+#Exercise 7 (Global variable)
+def myfunc():
+    global x
+    x = "fantastic"
+    print("Variable is " + x) 
+myfunc()
+print("Python is " + x)
+
+#Python Data Types
+
+#Exercise 1
+x = 5
+print(type(x))
+#Exercise 2
+x = "Hello world"
+print(type(x))
+#Exercise 3
+x = 20.5
+print(type(x))
+#Exercise 4
+x = ["apple", "banana", "cherry"]
+print(type(x))
+#Exercise 5
+x = ("apple", "banana", "cherry")
+print(type(x))
+#Exercise 6
+x = {"name" : "Jonh", "age" : 36}
+print(type(x))
+#Exercise 7
+x = True
+print(type(x))
+
+#Python numbers
+
+#Exercise 1
+x = 5
+x = float(x)
+#Exercise 2
+x = 5.5
+x = int(x)
+#Exercise 3
+x = 5
+x = complex(x)
