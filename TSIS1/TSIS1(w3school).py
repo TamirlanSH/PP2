66a05aa4283f7e5fc59dc9022ad6c95cd610a4e1
TamirlanSH <you@example.com>
2021-06-27 21:43:38 +0600

    New commit

--- TSIS1/TSIS1(w3school).py
+++ TSIS1/TSIS1(w3school).py
@@ -82,13 +82,58 @@ x = complex(x)
 
 #Python strings
 
-#Exercise 1
+#Exercise 1 length of x
 x = "Hello World"
 print(len(x))
-#Exercise 2
-#Exercise 3
-#Exercise 4
-#Exercise 5
+#Exercise 2 "H"
+txt = "Hello World"
+x = txt[0]
+#Exercise 3 "llo "
+txt = "Hello World"
+x = txt[2:5]
+#Exercise 4 remove all whitespace at begin and at end
+txt = " Hello World"
+x = txt.strip()
+#Exercise 5 to upper
+txt = "Hello World"
+txt = txt.upper()
 #Exercise 6
+txt = "Hello World"
+txt = txt.lower()
 #Exercise 7
+txt = "Hello World"
+txt = txt.replace("H", "J")
 #Exercise 8
+age = 36
+txt = "My name is Jonh, and I am {}"
+print(txt.format(age))
+
+#Python Booleans
+
+#Exercise 1 True
+print(10 > 9)
+#Exercise 2 False
+print(10 == 9)
+#Exercise 3 False
+print(10 < 9)
+#Exercise 4 True
+print(bool("abc"))
+#Exercise 5 False
+print(bool(0))
+
+#Python Operators
+
+#Exercise 1 Multiplication
+print(10 * 5)
+#Exercise 2 Divide
+print(10 / 2)
+#Exercise 3
+fruits = ["apple", "banana"]
+if "apple" in fruits:
+    print("Yes, apple is a fruit")
+#Exercise 4
+if 5 != 10:
+    print("5 and 10 is not equal")
+#Exercise 5
+if 5 == 10 or 4 == 4:
+    print("At least one of the statements is true")
